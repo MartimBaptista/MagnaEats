c7d87669e2ec91245c03503d2b634b06163a8d52
MartimBaptista <martimdsbaptista@gmail.com>
2022-04-05 00:06:24 +0100

    00:00 work is the best work, create_blabla done!

--- src/main.c
+++ src/main.c
@@ -1,9 +1,13 @@
 #include "main.h"
 #include "process.h"
+#include "memory.h"
+#include "restaurant.h"
+#include "driver.h"
+#include "client.h"
 
-// #include "/home/martin/Documents/MagnaEats/MagnaEats/include/memory.h" //so that vs can dettect the erros TO REMOVE!!!!!!
-// #include "/home/martin/Documents/MagnaEats/MagnaEats/include/main.h"
-// #include "/home/martin/Documents/MagnaEats/MagnaEats/include/process.h"
+ #include "/home/martin/Documents/MagnaEats/MagnaEats/include/memory.h" //so that vs can dettect the erros TO REMOVE!!!!!!
+ #include "/home/martin/Documents/MagnaEats/MagnaEats/include/main.h"
+ #include "/home/martin/Documents/MagnaEats/MagnaEats/include/process.h"
 
 
 #include <stdlib.h>
@@ -11,12 +15,10 @@
 #include <string.h>
 
 void main_args(int argc, char* argv[], struct main_data* data) {
-    /* remove this or implement as feature
-    if (argc != 6)
-        TODO?
-        printf("Nao colocou o numero argumentos certo, tente novamente \n");
+    if (argc != 6){
+        printf("Não colocou o numero argumentos certo, tente novamente.\n");
         exit(0);
-    */
+    }
    //Putting args in struct "data"
    data->max_ops = atoi(argv[1]);
    data->buffers_size = atoi(argv[2]);
@@ -66,31 +68,33 @@ void launch_processes(struct communication_buffers* buffers, struct main_data* d
 void user_interaction(struct communication_buffers* buffers, struct main_data* data){
     char interaction[20];
     int counter;
+    printf("Ações disponíveis:\n");
+    printf("request (client) (restaurant) (dish) - Criar um novo pedido\n");
+    printf("status (id) - Consultar o estado de um pedido\n");
+    printf("stop - Termina a execução do magnaeats.\n");
+    printf("help - Imprime informação sobre as ações disponíveis.\n");
+
     while (data->terminate != 0){
-        printf("O que deseja? escreva help para obter informacoes \n");
+        printf("Introduzir ação:\n");
         scanf("%s", interaction);
         if (strcmp("request", interaction) == 0) {
             create_request(&counter , buffers, data);
         }
         else if (strcmp("status", interaction) == 0) {
             read_status(data);
-            user_interaction(buffers, data);
         }
         else if (strcmp("stop", interaction) == 0) {
             stop_execution(data, buffers);
         }
         else if (strcmp("help", interaction) == 0) {
-            printf("Existem os seguintes comandos: \n");
-            printf("request - cria nova operacao, exemplo: request (id do cliente) (id do restaurante) (prato) \n");
-            printf("status - verifica o estado de uma operação, exemplo: status (id da operacao) \n"); 
-            printf("stop - termina a execucao do sistema MagnaEats \n");
-            printf("help - o que acabou de pedir (: \n");
-            user_interaction(buffers, data);
+            printf("Ações disponíveis:\n");
+            printf("request (client) (restaurant) (dish) - Criar um novo pedido\n");
+            printf("status (id) - Consultar o estado de um pedido\n");
+            printf("stop - Termina a execução do magnaeats.\n");
+            printf("help - Imprime informação sobre as ações disponíveis.\n");
         }
         else {
-            // mau input try again
-            printf("Input nao aceite, tente novamente \n");
-            user_interaction(buffers, data);
+            printf("Input não aceite, tente novamente.\n");
         }
     }
 }
@@ -98,12 +102,20 @@ void user_interaction(struct communication_buffers* buffers, struct main_data* d
 void create_request(int* op_counter, struct communication_buffers* buffers, struct main_data* data){
     if(*op_counter < data->max_ops){
         struct operation new_operation;
+        int client;
+        int rest;
+        char dish[20];
+        scanf("%d %d %s", &client, &rest, dish);
         new_operation.id = *op_counter;
-        scanf("");
-        //TODO increase counter        
+        new_operation.requesting_client = client;
+        new_operation.requested_rest = rest;
+        new_operation.requested_dish = dish;
+        new_operation.status = 'I';                                                                      //TODO IS IT I???????
+        write_main_rest_buffer(buffers->main_rest, data->buffers_size, &new_operation);
+        *op_counter++;
     }
     else
-        printf("Max operation reached!!!");
+        printf("Numero maximo de operações alcançado!!!");
 }
 
 void read_status(struct main_data* data){
@@ -141,18 +153,19 @@ void destroy_memory_buffers(struct main_data* data, struct communication_buffers
         destroy_dynamic_memory(data->client_stats);
 
     //Shared memory destruction
+        size_t op_buffer_size = data->buffers_size * sizeof(struct operation);
         //MAIN -> REST
-        destroy_shared_memory(STR_SHM_MAIN_REST_BUFFER, buffers->main_rest->buffer, data->buffers_size * sizeof(buffers->main_rest->buffer));
-        destroy_shared_memory(STR_SHM_MAIN_REST_PTR, buffers->main_rest->ptrs, data->buffers_size * sizeof(buffers->main_rest->ptrs));
+        destroy_shared_memory(STR_SHM_MAIN_REST_BUFFER, buffers->main_rest->buffer, op_buffer_size);
+        destroy_shared_memory(STR_SHM_MAIN_REST_PTR, buffers->main_rest->ptrs, data->buffers_size * sizeof(int));
         //REST -> DRIV
-        destroy_shared_memory(STR_SHM_REST_DRIVER_BUFFER, buffers->rest_driv->buffer, data->buffers_size * sizeof(buffers->rest_driv->buffer));
-        destroy_shared_memory(STR_SHM_REST_DRIVER_PTR, buffers->rest_driv->ptrs, data->buffers_size * sizeof(buffers->rest_driv->ptrs));
+        destroy_shared_memory(STR_SHM_REST_DRIVER_BUFFER, buffers->rest_driv->buffer, op_buffer_size);
+        destroy_shared_memory(STR_SHM_REST_DRIVER_PTR, buffers->rest_driv->ptrs, data->buffers_size * sizeof(struct pointers));
         //DRIV -> CLI
-        destroy_shared_memory(STR_SHM_DRIVER_CLIENT_BUFFER, buffers->driv_cli->buffer, data->buffers_size * sizeof(buffers->driv_cli->buffer));
-        destroy_shared_memory(STR_SHM_DRIVER_CLIENT_PTR, buffers->driv_cli->ptrs, data->buffers_size * sizeof(buffers->driv_cli->ptrs));
+        destroy_shared_memory(STR_SHM_DRIVER_CLIENT_BUFFER, buffers->driv_cli->buffer, op_buffer_size);
+        destroy_shared_memory(STR_SHM_DRIVER_CLIENT_PTR, buffers->driv_cli->ptrs, data->buffers_size * sizeof(int));
         //results and terminate
-        destroy_shared_memory(STR_SHM_RESULTS, data->results, data->buffers_size * sizeof(data->results));
-        destroy_shared_memory(STR_SHM_TERMINATE, data->terminate, data->buffers_size * sizeof(data->terminate));
+        destroy_shared_memory(STR_SHM_RESULTS, data->results, data->max_ops * sizeof(struct operation));
+        destroy_shared_memory(STR_SHM_TERMINATE, data->terminate, sizeof(int));
 }
 
 int main(int argc, char *argv[]) { 
