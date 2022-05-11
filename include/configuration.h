71de8feed5e4d81ba37ebac4961c88cf84800e2d
joaoinca <joaoinca007@gmail.com>
2022-05-11 14:45:06 +0100

    config, find out wtf to do with new 3 shits

--- include/configuration.h
+++ include/configuration.h
@@ -3,12 +3,6 @@
 #define FILENAME_SIZE (256)
 
 struct configMagna { 
-    int max_ops;			//número máximo de operações
-	int buffers_size;		//tamanho máximo dos buffers de mem. partilhada
-
-	int n_restaurants;		//número de restaurantes
-	int n_drivers;			//número de motoristas
-	int n_clients;			//número de clientes
 
     char log_filename[FILENAME_SIZE];                //nome do ficheiro de log
     char statistics_filename[FILENAME_SIZE];         //nome do ficheiro de estatísticas
@@ -16,4 +10,4 @@ struct configMagna {
 
 };
 
-void configRead(char* argv[], struct main_data* data);
+void configRead(char* argv[], struct main_data* data, struct configMagna* configM);
--- src/configuration.c
+++ src/configuration.c
@@ -1,8 +1,61 @@
 #include <stdio.h>
 #include <configuration.h>
+#include "memory.h"
+#define LINE_SIZE 256
 
-void configRead(char* argv[], struct main_data* data) {
+void configRead(char* argv[], struct main_data* data, struct configMagna* configM) {
 
-    struct configMagna conf;
+    char linha[LINE_SIZE];
+    char arg1[LINE_SIZE];
+    // ugly
+    int scanNum;
 
+    FILE *configFile;
+    configFile = fopen("config.txt", "r");
+    if (configFile == NULL){
+        perror("Ficheiro de dados.");
+        exit(1);
+    }
+    while (fgets(linha, LINE_SIZE, configFile) != NULL){
+        if (sscanf(linha, "%*s %s %*s", arg1) != 1){
+            printf("erro");
+            exit(2);
+        }
+        else {
+            //ugly part TODO make it better
+            if (scanNum == 0) {
+                data->max_ops = arg1;
+                scanNum++;
+            }
+            else if (scanNum == 1) {
+                data->buffers_size = arg1;
+                scanNum++;
+            }
+            else if (scanNum == 2) {
+                data->n_restaurants = arg1;
+                scanNum++;
+            }
+            else if (scanNum == 3) {
+                data->n_drivers = arg1;
+                scanNum++;
+            }
+            else if (scanNum == 4) {
+                data->n_clients = arg1;
+                scanNum++;
+            }
+            else if (scanNum == 5) {
+                configM->log_filename = arg1;
+                scanNum++;
+            }
+            else if (scanNum == 6) {
+                configM->statistics_filename = arg1;
+                scanNum++;
+            }
+            else if (scanNum == 7) {
+                configM->alarm_time = atof(arg1);
+                scanNum++;
+            }
+        }
+
+    }
 }
