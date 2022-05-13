b695884d7d93ec518411660c5f26964f16be511b
MartimBaptista <martimdsbaptista@gmail.com>
2022-05-13 17:43:21 +0100

    I give up for today, fuck this

--- include/metime.h
+++ include/metime.h
@@ -1,8 +1,11 @@
 #ifndef TIME_H_GUARD
 #define TIME_H_GUARD
 
-struct timespec{
-    int idk;
-};
+//resgister a timespec to the moment this was called
+void register_timespec(struct timespec* time);
+
+//calc the total time (for stats.h)
+void calc_total(struct timespec* start, struct timespec* end);
+
 
 #endif
--- src/metime.c
+++ src/metime.c
@@ -1,1 +1,11 @@
-//KONO DIO DA
+#include "metime.h"
+
+#include <time.h>
+
+void register_timespec(struct timespec* time){
+    return;
+}
+
+void calc_total(struct timespec* start, struct timespec* end){
+    return;
+}
